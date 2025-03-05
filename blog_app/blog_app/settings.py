a67229c5bb396aa77ad27ea7ba980afbc5885839
Elbin12 <elbinshiby14@gmail.com>
2025-03-05 17:31:33 +0530

    changed allowed hosts

--- blog_app/blog_app/settings.py
+++ blog_app/blog_app/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'blog.elbin.site']
+ALLOWED_HOSTS = ['localhost', 'blog.elbin.live']
 
 
 # Application definition
